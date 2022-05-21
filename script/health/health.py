799755f19b207ed2597ea1352873a1ccf3b8f1b0
Grant Birkinbine <grantbirki@github.com>
2022-05-21 12:44:20 -0600

    add healthcheck script

---
+++ script/health/health.py
@@ -0,0 +1,39 @@
+import sys
+import time
+
+from requests_html import HTMLSession
+
+URL = "https://status.tarkov.dev"
+TEXT_SEARCH = "https://github.com/the-hideout/status"
+RETRY_SLEEP = 2
+
+
+def check():
+    try:
+        s = HTMLSession()
+        response = s.get(URL)
+        response.html.render()
+
+        if TEXT_SEARCH in response.html.text:
+            return True
+        else:
+            print(response.html.text)
+            return False
+    except:
+        return False
+
+
+def main():
+    for i in range(0, 5):
+        counter = i + 1
+        if check():
+            print(f"[{counter}] health: OK")
+            sys.exit(0)
+        else:
+            time.sleep(RETRY_SLEEP)
+            print(f"[{counter}] health: FAIL")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
